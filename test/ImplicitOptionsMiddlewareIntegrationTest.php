9c074ff5f3fe5c2dd074a9ffe43f52a8e9895e3f
webimpress <contact@webimpress.com>
2018-03-01 19:54:28 +0000

    Added ImplicitOptionsMiddlewareIntegrationTest
    Check if all allowed methods are included in the Allow header for OPTIONS
    request

---
+++ test/ImplicitOptionsMiddlewareIntegrationTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-expressive-fastroute for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive-fastroute/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace ZendTest\Expressive\Router;
+
+use PHPUnit\Framework\TestCase;
+use Zend\Expressive\Router\FastRouteRouter;
+use Zend\Expressive\Router\RouterInterface;
+use ZendTest\Expressive\Router\Middleware\ImplicitOptionsMiddlewareIntegrationTestTrait;
+
+class ImplicitOptionsMiddlewareIntegrationTest extends TestCase
+{
+    use ImplicitOptionsMiddlewareIntegrationTestTrait;
+
+    public function getRouter() : RouterInterface
+    {
+        return new FastRouteRouter();
+    }
+}
